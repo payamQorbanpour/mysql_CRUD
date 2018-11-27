d74847563417ab0f2b60af679fb052f1145c773d
payamQorbanpour <payamqorbanpour@gmail.com>
2018-11-27 09:45:14 +0330

    READ all data of tables from mysqldb.

--- mysql_crud.py
+++ mysql_crud.py
@@ -1,32 +1,23 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api, abort
 from marshmallow import fields, Schema, ValidationError
-from pymongo import MongoClient
-import pymysql
+from sqlalchemy import create_engine, MetaData, select, Table
 
 
-# db connections
-db = pymysql.connect("localhost",
-                     "john",
-                     "",
-                     "TESTDB")
-cursor = db.cursor()
-cursor.execute("SELECT VERSION()")
-data = cursor.fetchone()
-print ("Database version: %s" % data)
-db.close()
+# Defining database
+engine = create_engine("mysql+mysqldb://john:1234@localhost/TESTDB")
+conn = engine.connect()
+metadata = MetaData()
+stuff = Table('stuff', metadata, autoload=True, autoload_with=engine)
 
 app = Flask(__name__)
 api = Api(app)
 
-# Defining database and collection named stuff
-all_stuff = MongoClient().crud_db.stuff
 
-
-def does_exist(stuff_id):
-    this_stuff = all_stuff.find_one({"data": stuff_id})
-    if not this_stuff:
-        abort(404, message="doesn't exist.")
+# def does_exist(stuff_id):
+#     this_stuff = all_stuff.find_one({"data": stuff_id})
+#     if not this_stuff:
+#         abort(404, message="doesn't exist.")
 
 
 def price_limitaion(price):
@@ -42,48 +33,47 @@ class Input(Schema):
     price = fields.Float(validate=price_limitaion)
     email = fields.Email()
 
-
-class Stuff(Resource):
-    # READ individually
-    def get(self, stuff_id):
-        this_stuff = all_stuff.find_one({"data": stuff_id}, {"_id": 0})
-        does_exist(stuff_id)
-        return this_stuff
-
-    # CREATE
-    def post(self, stuff_id):
-        stuff = request.get_json()
-        schema = Input()
-        result = schema.load(stuff)
-        all_stuff.insert_one(stuff)
-        return result, 201
-
-    # UPDATE
-    def put(self, stuff_id):
-        stuff = request.get_json()
-        schema = Input()
-        result = schema.load(stuff)
-        all_stuff.update_one({"data": stuff_id}, {"$set": stuff})
-        return result, 201
-
-    # DELETE
-    def delete(self, stuff_id):
-        does_exist(stuff_id)
-        all_stuff.delete_one({"data": stuff_id})
-        return 'Data deleted!', 204
+#
+# class Stuff(Resource):
+#     # READ individually
+#     def get(self, stuff_id):
+#         this_stuff = all_stuff.find_one({"data": stuff_id}, {"_id": 0})
+#         does_exist(stuff_id)
+#         return this_stuff
+#
+#     # CREATE
+#     def post(self, stuff_id):
+#         stuff = request.get_json()
+#         schema = Input()
+#         result = schema.load(stuff)
+#         all_stuff.insert_one(stuff)
+#         return result, 201
+#
+#     # UPDATE
+#     def put(self, stuff_id):
+#         stuff = request.get_json()
+#         schema = Input()
+#         result = schema.load(stuff)
+#         all_stuff.update_one({"data": stuff_id}, {"$set": stuff})
+#         return result, 201
+#
+#     # DELETE
+#     def delete(self, stuff_id):
+#         does_exist(stuff_id)
+#         all_stuff.delete_one({"data": stuff_id})
+#         return 'Data deleted!', 204
 
 
 # READ totally
 class AllStuff(Resource):
     def get(self):
-        stuff_bucket = []
-        cursor = all_stuff.find({}, {"_id": 0})
-        for result in cursor:
-            stuff_bucket.append(result)
-        return stuff_bucket
+        table = select([stuff])
+        result = conn.execute(table).fetchall()
+        stringish = str(result)
+        return stringish
 
 
-api.add_resource(Stuff, '/<string:stuff_id>')
+# api.add_resource(Stuff, '/<string:stuff_id>')
 api.add_resource(AllStuff, '/')
 
 if __name__ == '__main__':
